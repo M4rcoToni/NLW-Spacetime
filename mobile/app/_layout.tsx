e3c576b5cedc409153fbf7fa265f9130c13da0c7
M4rcoToni <marcosanto.sl021@academico.ifsul.edu.br>
2023-05-19 13:12:35 -0300

    style: layout

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,59 @@
+import { styled } from 'nativewind'
+import { Stack, SplashScreen } from 'expo-router'
+import { ImageBackground } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import { useEffect, useState } from 'react'
+import * as SecureStore from 'expo-secure-store'
+
+const StyledStripes = styled(Stripes)
+
+export default function Layout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<
+    null | boolean
+  >(null)
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) =>
+      setIsUserAuthenticated(!!token),
+    )
+  }, [])
+
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return <SplashScreen />
+  }
+
+  return (
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 bg-gray-900 "
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-0" />
+      <StatusBar style="light" translucent />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: 'transparent' },
+        }}
+      >
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="memories" />
+      </Stack>
+    </ImageBackground>
+  )
+}
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,25 +1,10 @@
-import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { useRouter } from 'expo-router'
+import { Text, TouchableOpacity, View } from 'react-native'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 import * as SecureStore from 'expo-secure-store'
-import { styled } from 'nativewind'
-
-import {
-  Roboto_400Regular,
-  Roboto_700Bold,
-  useFonts,
-} from '@expo-google-fonts/roboto'
-
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
-import blurBg from '../src/assets/bg-blur.png'
 import NlwLogo from '../src/assets/nlw-spacetime-logo.svg'
-import Stripes from '../src/assets/stripes.svg'
-import { useEffect } from 'react'
 import { api } from '../src/lib/api'
-import { useRouter } from 'expo-router'
-
-const StyledStripes = styled(Stripes)
 
 // Endpoint
 const discovery = {
@@ -31,11 +16,7 @@ const discovery = {
 
 export default function App() {
   const router = useRouter()
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
+
   const [, response, signInInWithGitHub] = useAuthRequest(
     {
       clientId: '7d0ff78f96dfa6de59d3',
@@ -64,19 +45,8 @@ export default function App() {
     }
   }, [response])
 
-  if (!hasLoadedFonts) {
-    return null
-  }
-  console.log(response)
-
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-0" />
-
+    <View className=" flex-1 items-center  px-8 py-10">
       <View className=" flex-1 items-center justify-center gap-6">
         <NlwLogo />
         <View className="space-y-2">
@@ -101,7 +71,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   )
 }
